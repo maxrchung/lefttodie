62b5d8d2c85445eccaa8c55bbd05201ed4bf73c8
Andrew <aschau@uci.edu>
2015-01-17 13:03:32 -0800

    SpriteSheets load complete. Animate is being changed to support one image at a time.

--- Animation.py
+++ Animation.py
@@ -4,21 +4,74 @@ from pygame.locals import *
 
 
 #Class to load spritesheet or parts of the spritesheet
-class SpriteSheet(object):
-    def __init__(self, file):
+class SpriteSheets():
+    def __init__(self, folder):
         try:
-            self.sheet = pygame.image.load(os.path.join('Sprites', file)).convert()
+            self.folder = folder
+            os.path.isdir(self.folder)
 
-        except pygame.error, message:
-            print ('Unable to load spritesheet', file)
-            raise SystemExit, message
+        except pygame.error:
+            print ('Unable to load art folder', folder)
+            raise SystemExit
 
-    def crop(self, x, y, width, height):
-        crop = pygame.Rect(x, y, width, height)
-        sprite = pygame.Surface(crop.size).convert()
-        sprite.blit(self.sheet, (0,0), crop)
-        return sprite
+    def load(self, image):
+        self.sheet = pygame.image.load(os.path.join(self.folder, image))
 
-    def sequence(self, rect, num_images):
-        sprites = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3]) 
+    def loadAll(self):
+        self.all = {}
+        for root, dirs, files in os.walk(self.folder):
+           for image in files:
+               if image[-3:] == "png":
+                   self.all[image] = pygame.image.load(os.path.join(self.folder, image))
+        return self.all
         
+
+
+w = 32
+h = 32
+window = pygame.display.set_mode((w, h))
+frame = -1
+clock = pygame.time.Clock()
+columns = 2
+
+s = SpriteSheets("Art")
+#s.load("playerIdleNormal.png")
+a = s.loadAll()
+print(a)
+a["playerIdleNormal.png"]
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit();
+            sys.exit();
+    window.fill((255, 255, 255))
+    frame += 1;
+    if frame > 1:
+        frame = 0
+
+    window.blit(a["playerIdleNormal.png"], (0, 0), ((frame % columns) * w, 0, w, h))
+    pygame.display.update()
+    clock.tick(5)
+
+class Animate():
+    def __init__(self, image):
+        try:
+            self.image = image
+            self.frame = -1
+            
+        except pygame.error:
+            print ('Unable to load images')
+            raise SystemExit
+
+    def Aupdate(self, frames, columns, int timer):
+            self.frame += 1
+            if self.frame > frames - 1:
+                self.frame = 0
+
+            window.blit(a["playerIdleNormal.png"], (0, 0), ((self.frame % columns) * w, 0, w, h))
+            pygame.display.update()
+            clock.tick(timer)
+
+    def draw(self):
+        pass
