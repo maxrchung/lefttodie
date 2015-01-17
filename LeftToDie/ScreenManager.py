e14e0c467793024578f0c6f8ffe1b5ba69f43aee
hectoram <hectoram@uci.edu>
2015-01-17 15:25:59 -0800

    Added name to screen. Can now use X to close program.

--- LeftToDie/ScreenManager.py
+++ LeftToDie/ScreenManager.py
@@ -3,13 +3,16 @@ import pygame
 
 class Screen:
 
+    pygame.init()
     screen = pygame.display
+    background_colour = (255, 255, 255)
 
     def __init__(self):
         self.screen = pygame.display.set_mode((1024, 768))
+        pygame.display.set_caption('Left To Die')
 
     def update(self):
         self.draw()
 
     def draw(self):
-        print("I'm drawing")
+        print("Hello")
