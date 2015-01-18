103a236c0ff5d35d8f39b7836c6e185c42fc6b44
maxrc <maxrc@uci.edu>
2015-01-17 18:47:55 -0800

    Edited soundmanager code to play all sounds

--- LeftToDie/soundmanager.py
+++ LeftToDie/soundmanager.py
@@ -5,7 +5,7 @@ class soundmanager():
         pygame.mixer.init(frequency = 441100, size = -16, channels = 2, buffer = 4096)
 
         # Preload sounds so we don't run into processing issues
-        self.backgroundMusic = pygame.mixer.music.load("Sounds/syobon.mp3")
+        self.syobon = pygame.mixer.music.load("Sounds/syobon.mp3")
         self.jumpSound = pygame.mixer.Sound("Sounds/jump.wav")
         self.deathSound = pygame.mixer.Sound("Sounds/death.wav")
         self.levelDieSound = pygame.mixer.Sound("Sounds/levelDie.wav")
@@ -14,9 +14,6 @@ class soundmanager():
 
         # Loop music
         pygame.mixer.music.play(-1)
-
-    def update(self):
-        pass
 
     def playsound(self, soundname):
         if soundname == "jump":
@@ -29,5 +26,9 @@ class soundmanager():
             self.levelUpSound.play()
         elif soundname == "inverse":
             self.inverseSound.play(-1)
+            pygame.mixer.music.pause()
+        elif soundname == "syobon":
+            self.inverseSound.stop()
+            pygame.mixer.music.play(-1)
         else:
             print("Can't locate sound file.")
