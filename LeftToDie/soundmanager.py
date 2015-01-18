1cf382c04749131ff4e58565f51e96407fd9c7a7
maxrc <maxrc@uci.edu>
2015-01-17 19:19:26 -0800

    Fixed some bugs with soundmanager, sounds should be able to be played without problems now

--- LeftToDie/soundmanager.py
+++ LeftToDie/soundmanager.py
@@ -2,14 +2,14 @@ import pygame
 
 class soundmanager():
     def __init__(self):
-        pygame.mixer.init(frequency = 441100, size = -16, channels = 2, buffer = 4096)
+        pygame.mixer.init(frequency = 44100, size = -16, channels = 2, buffer = 4096)
 
         # Preload sounds so we don't run into processing issues
         self.syobon = pygame.mixer.music.load("Sounds/syobon.mp3")
         self.jumpSound = pygame.mixer.Sound("Sounds/jump.wav")
         self.deathSound = pygame.mixer.Sound("Sounds/death.wav")
         self.levelDieSound = pygame.mixer.Sound("Sounds/levelDie.wav")
-        self.levelDieSound = pygame.mixer.Sound("Sounds/levelUp.wav")
+        self.levelUpSound = pygame.mixer.Sound("Sounds/levelUp.wav")
         self.inverseSound = pygame.mixer.Sound("Sounds/inverse.wav")
 
         # Loop music
@@ -29,6 +29,6 @@ class soundmanager():
             pygame.mixer.music.pause()
         elif soundname == "syobon":
             self.inverseSound.stop()
-            pygame.mixer.music.play(-1)
+            pygame.mixer.music.unpause()
         else:
             print("Can't locate sound file.")
