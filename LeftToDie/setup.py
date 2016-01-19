2c8e30d2b11e9c373b4b1e23534b668bdb633225
Andrew <aschau@uci.edu>
2016-01-19 11:32:00 -0800

    added LeftToDie Fixed.zip

--- LeftToDie/setup.py
+++ LeftToDie/setup.py
@@ -3,6 +3,6 @@ from cx_Freeze import setup, Executable
 
 
 setup(name = 'Left to Die', 
-      version='4.2.0.', 
+      version=.'1.0', 
       description='Right to live',
       executables = [Executable(script = 'LeftToDie.py', base='Win32GUI')])
